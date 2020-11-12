a08a6286d3d8e99ba0788ce4613cd71c320af25d
Ivan Solovyov <iasolovyovwork@gmail.com>
2020-11-12 21:25:31 +0300

    Add ability to add timing function to animation on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,6 +15,27 @@ const Home = ({ isLoaded }) => {
 
   const [isLoadedState, setIsLoadedState] = React.useState(false);
 
+  const timingFunc = React.useCallback(timeFraction => {
+    return 1 - Math.sin(Math.acos(timeFraction));
+  }, []);
+
+  const animate = React.useCallback(({ timing, draw, duration }) => {
+    const start = performance.now();
+
+    requestAnimationFrame(function animates(time) {
+      let timeFraction = (time - start) / duration;
+      if (timeFraction > 1) timeFraction = 1;
+
+      const progress = timing(timeFraction);
+
+      draw(progress);
+
+      if (timeFraction < 1) {
+        requestAnimationFrame(animates);
+      }
+    });
+  }, []);
+
   React.useEffect(() => {
     const ctx = canvasRef.current.getContext('2d');
     let widthCanvas = window.innerWidth;
@@ -22,16 +43,18 @@ const Home = ({ isLoaded }) => {
     let height = heightCanvas + 200;
     const textWidth = 700;
 
-    const drawRect = () => {
+    const drawRect = progress => {
       const rectWidth = widthCanvas / 3 + 1;
 
       ctx.clearRect(0, 0, widthCanvas, heightCanvas);
 
+      height = (1 - progress) * (heightCanvas + 200);
+
       ctx.save();
       ctx.fillStyle = '#191919';
-      ctx.fillRect(0, 0, rectWidth, height - 200);
-      ctx.fillRect(rectWidth - 1, 0, rectWidth * 2, height - 100);
-      ctx.fillRect(rectWidth * 2 - 1, 0, rectWidth * 3, height);
+      ctx.fillRect(0, 0, rectWidth + 1, height - 200);
+      ctx.fillRect(rectWidth - 1, 0, rectWidth * 2 - 1, height - 100);
+      ctx.fillRect(rectWidth * 2 - 1, 0, rectWidth * 3 - 1, height);
       ctx.restore();
 
       ctx.save();
@@ -46,27 +69,39 @@ const Home = ({ isLoaded }) => {
       );
       ctx.restore();
 
-      if (loading && isLoadedState && height > 0) {
-        height -= 8;
-        window.requestAnimationFrame(drawRect);
-      }
+      console.log(progress);
+      // if (loading && isLoadedState && height > 0) {
+      //   height -= 7;
+      //   window.requestAnimationFrame(drawRect);
+      // }
     };
+
+    if (loading && isLoadedState) {
+      animate({
+        duration: 1000,
+        timing: timingFunc,
+        draw: drawRect,
+      });
+    }
 
     const resizeCanvas = () => {
       canvasRef.current.width = widthCanvas = window.innerWidth;
       canvasRef.current.height = heightCanvas = window.innerHeight;
-      drawRect();
+      if (height === 0) {
+        drawRect(1);
+      } else {
+        drawRect(0);
+      }
     };
 
     resizeCanvas();
-    drawRect();
 
     window.addEventListener('resize', resizeCanvas, false);
 
     return () => {
       window.removeEventListener('resize', resizeCanvas, false);
     };
-  }, [canvasRef, loading, isLoadedState]);
+  }, [canvasRef, loading, isLoadedState, animate, timingFunc]);
 
   React.useEffect(() => {
     if (isLoaded) {
