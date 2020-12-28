81e35253804e0640a6cc63523052292682053702
Ivan Solovyov <iasolovyovwork@gmail.com>
2020-12-28 03:07:31 +0300

    Add math fit

--- src/pages/Home/Card/Card.js
+++ src/pages/Home/Card/Card.js
@@ -2,37 +2,41 @@ import * as PIXI from 'pixi.js';
 import carImg from 'images/car.jpg';
 import backgroundImg from 'images/background.jpg';
 import shader from './shader.frag';
-import waterTexture from 'images/displacement_map_repeat.jpg';
+import fit from 'math-fit';
 
 export default class Card {
   constructor(container) {
     this.canvasContainer = container;
-    this.height = window.innerHeight * 0.7;
-    this.width = window.innerWidth * 0.6;
+    this.height = window.innerHeight;
+    this.width = window.innerWidth;
     this.mouse = { x: 1, y: 1 };
-
-    console.log('size', window.innerWidth * 0.6, window.innerHeight * 0.7);
 
     this.app = new PIXI.Application({
       resolution: window.devicePixelRatio || 1,
       width: this.width,
       height: this.height,
+      backgroundColor: 0xf1efe5,
     });
 
     this.canvasContainer.appendChild(this.app.view);
 
     this.container = new PIXI.Container();
-    this.container.width = this.width;
-    this.container.height = this.height;
+
+    this.rectangleContainer = new PIXI.Container();
 
     this.app.stage.addChild(this.container);
+    this.app.stage.addChild(this.rectangleContainer);
 
     this.loader = new PIXI.Loader();
+
+    this.rectangle = new PIXI.Graphics()
+      .beginFill(0xffffff)
+      .drawRect(0, 0, window.innerWidth, window.innerHeight)
+      .endFill();
 
     this.loader
       .add('car', carImg)
       .add('background', backgroundImg)
-      .add('waterTexture', waterTexture)
       .load(this.setup);
 
     this.app.stop();
@@ -44,71 +48,89 @@ export default class Card {
     });
   }
 
-  pointerMove = event => {
-    this.mouse.x =
-      ((event.data.global.x - this.width / 2) / window.innerWidth) * 1.5;
-    this.mouse.y =
-      (-event.data.global.y + this.height / 2) / window.innerHeight;
-  };
-
   destroyListener = () => {
     window.removeEventListener('resize', this.resize);
   };
 
   resize = () => {
-    this.height = window.innerHeight * 0.7;
-    this.width = window.innerWidth * 0.6;
-    this.app.renderer.resize(this.width, this.height);
+    this.height = window.innerHeight;
+    this.width = window.innerWidth;
 
-    const containerRatio = this.container.width / this.container.height;
-    const appRatio = this.app.stage.width / this.app.stage.height;
+    const coverValues = fit.cover(
+      { w: this.carTexture.width, h: this.carTexture.height },
+      { w: this.width * 0.6, h: this.height * 0.7 }
+    );
 
-    console.log(this.width, this.container.width);
+    this.rectangleContainer.width = this.width * 0.6;
+    this.rectangleContainer.height = this.height * 0.7;
 
-    if (appRatio > containerRatio) {
-      console.log('bigger');
-      this.container.height =
-        this.container.height / (this.container.width / this.container.width);
-      this.container.width = this.container.width;
-      this.container.position.x = 0;
-      this.container.position.y =
-        (this.container.height - this.container.height) / 2;
-    } else {
-      console.log('smaller');
-      this.container.width =
-        this.container.width / (this.container.height / this.container.height);
-      this.container.height = this.container.height;
-      this.container.position.y = 0;
-      this.container.position.x =
-        (this.container.width - this.container.width) / 2;
-    }
+    this.rectangleContainer.x =
+      (this.width - this.rectangleContainer.width) / 2;
+    this.rectangleContainer.y =
+      (this.height - this.rectangleContainer.height) / 2;
 
-    this.carPicture.width = this.container.width;
-    this.carPicture.height = this.container.height;
+    this.rectangleSprite.width = coverValues.width;
+    this.rectangleSprite.height = coverValues.height;
 
-    console.log(this.width, this.container.width);
+    this.container.x = (this.width - this.rectangleContainer.width) / 2;
+    this.container.y = (this.height - this.rectangleContainer.height) / 2;
+
+    this.container.width = coverValues.width;
+    this.container.height = coverValues.height;
+    // this.carPicture.height = this.container.height;
+
+    console.log('resize');
+    // this.carPicture.pivot.x = this.carPicture.width / 2;
+    // this.carPicture.pivot.y = this.carPicture.height / 2;
+    // this.carPicture.anchor.set(1 - coverValues.scale);
+  };
+
+  pointerMove = event => {
+    this.mouse.x =
+      ((event.data.global.x - this.container.width / 2) / window.innerWidth) *
+      1.5;
+    this.mouse.y =
+      (-event.data.global.y + this.container.height / 2) / window.innerHeight;
   };
 
   setup = (_, res) => {
-    this.carPicture = PIXI.Sprite.from(res.car.texture);
+    this.carTexture = new PIXI.Texture.from(res.car.url);
+    this.carPicture = new PIXI.Sprite.from(this.carTexture);
+
+    this.recTexture = this.app.renderer.generateTexture(
+      this.rectangle,
+      PIXI.SCALE_MODES.NEAREST,
+      1,
+      this.rectangle
+    );
+    this.rectangleSprite = new PIXI.Sprite.from(this.recTexture);
+
     this.container.addChild(this.carPicture);
+
+    this.rectangleContainer.addChild(this.rectangleSprite);
 
     this.mouseFilter = new PIXI.Filter(null, shader, {
       u_time: 0,
       u_image: res.car.texture,
       u_imagehover: res.background.texture,
       u_res: {
-        x: this.width,
-        y: this.height,
+        x: this.container.width,
+        y: this.container.height,
       },
       u_mouse: this.mouse,
     });
 
     this.carPicture.filters = [this.mouseFilter];
 
+    this.container.mask = this.rectangleContainer;
+
     this.app.stage.interactive = true;
+    this.rectangleContainer.interactive = true;
 
     this.app.stage.on('mousemove', this.pointerMove);
+
+    this.resize();
+    this.resize();
 
     this.app.start();
   };
